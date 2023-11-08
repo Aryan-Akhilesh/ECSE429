578fa8428b45de5b6a1cbae6ac744d327deef80d
Chen Jun Chi <93779005+MosinLover@users.noreply.github.com>
2023-11-08 13:57:39 -0500

    story test 2

--- TodoListManagerTest/src/test/java/StoryTest/Category/CategoryStepDefinition.java
+++ TodoListManagerTest/src/test/java/StoryTest/Category/CategoryStepDefinition.java
@@ -34,6 +34,8 @@ public class CategoryStepDefinition {
         Response r = RestAssured.given().header("Content-Type", json).get("http://localhost:4567/categories/3");
         String body = "{\"categories\":[{\"id\":\"3\",\"title\":\"Train\",\"description\":\"\"}]}";
         Assertions.assertEquals(body, r.getBody().asString());
+        Assertions.assertEquals(200, r.getStatusCode());
+
     }
 
     @When("I use Post request to modify my existing category")
@@ -55,14 +57,70 @@ public class CategoryStepDefinition {
         RestAssured.given().header("Content-Type", json).body(jsonString).put("http://localhost:4567/categories/300");
     }
 
-    @Then("I should see that the title of the category is not modified")
-    public void i_should_see_the_title_not_modified() {
-        Response r = RestAssured.given().header("Content-Type", json).get("http://localhost:4567/categories/3");
-        String body = "{\"categories\":[{\"id\":\"3\",\"title\":\"Plane\",\"description\":\"\"}]}";
-        Assertions.assertEquals(body, r.getBody().asString());
+    @Then("I should be warned that id is invalid")
+    public void i_should_see_warning_on_invalid_id() {
+        String error = "{\"errorMessages\":[\"Invalid GUID for 300 entity category\"]}";
+        Assertions.assertEquals(error, response.getBody().asString());
+        Assertions.assertEquals(404, response.getStatusCode());
     }
 
     // User scenario 2
+
+    @Given("I have an existing category with valid field")
+    public void i_have_an_existing_category_with_valid_field() {
+        String jsonString = "{\"title\":\"School\",\"description\":\"university\"}";
+        RestAssured.given().header("Content-Type", json).body(jsonString).post("http://localhost:4567/categories");
+    }
+
+    @When("I replace the title and description field of the existing category")
+    public void i_replace_the_title_and_description_field_of_an_existing_category() {
+        String jsonString = "{\"title\":\"University\",\"description\":\"McGill\"}";
+        RestAssured.given().header("Content-Type", json).body(jsonString).put("http://localhost:4567/categories/3");
+    }
+
+    @Then("I should see the title and description of the category replaced")
+    public void i_should_see_the_category_title_description_field_replaced() {
+        Response r = RestAssured.given().header("Content-Type", json).get("http://localhost:4567/categories/3");
+        String body = "{\"categories\":[{\"id\":\"3\",\"title\":\"University\",\"description\":\"McGill\"}]}";
+        Assertions.assertEquals(body, r.getBody().asString());
+        Assertions.assertEquals(200, r.getStatusCode());
+    }
+
+    @When("I delete the current existing category and create a new one")
+    public void i_delete_current_existing_category_and_create_a_new_one() {
+        String jsonString = "{\"title\":\"University\",\"description\":\"McGill\"}";
+        RestAssured.given().header("Content-Type", json).delete("http://localhost:4567/categories/3");
+        RestAssured.given().header("Content-Type", json).body(jsonString).post("http://localhost:4567/categories");
+    }
+
+    @Then("I should see the title and description of the category replaced after deletion")
+    public void i_should_see_the_category_title_description_field_replaced_after_deletion() {
+        Response r = RestAssured.given().header("Content-Type", json).get("http://localhost:4567/categories/3");
+        String body = "{\"categories\":[{\"id\":\"3\",\"title\":\"University\",\"description\":\"McGill\"}]}";
+        Assertions.assertEquals(body, r.getBody().asString());
+        Assertions.assertEquals(200, r.getStatusCode());
+    }
+
+    @When("I replace the category with an invalid id")
+    public void i_replace_category_with_incorrect_id() {
+        String jsonString = "{\"title\":\"University\",\"description\":\"McGill\"}";
+        RestAssured.given().header("Content-Type", json).body(jsonString).put("http://localhost:4567/categories/300");
+    }
+
+    @Then("I should be warned that id is invalid")
+    public void i_should_see_warning_on_invalid_id_provided() {
+        String error = "{\"errorMessages\":[\"Invalid GUID for 300 entity category\"]}";
+        Assertions.assertEquals(error, response.getBody().asString());
+        Assertions.assertEquals(404, response.getStatusCode());
+    }
+
+    // User scenario 3
+    
+
+
+
+
+
 
 
 
