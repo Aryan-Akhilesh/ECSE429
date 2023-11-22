80db394f484308160b0df97b69713ddcd98db595
SHIMARIN <Justin.lyz@outlook.com>
2023-11-21 22:31:20 -0500

    Performance Test for Projects
    I push this just to make sure that it is in the right format.

--- TodoListManagerTest/src/test/java/PerformanceTest/ProjectsPerformanceTest.java
+++ TodoListManagerTest/src/test/java/PerformanceTest/ProjectsPerformanceTest.java
@@ -5,16 +5,14 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.json.JSONObject;
 import org.json.XML;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 import javax.xml.crypto.dsig.XMLObject;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
+@TestMethodOrder(MethodOrderer.Random.class)
 public class ProjectsPerformanceTest {
     private static final String pUrl = "http://localhost:4567/projects";
     private static ProcessBuilder pb;
@@ -60,7 +58,7 @@ public class ProjectsPerformanceTest {
         List<Long> timeTable = new ArrayList<Long>();
         for(int i=0;i<1000;i++){
             JSONObject body = new JSONObject();
-            String newTitle = "New project " + String.valueOf(i);
+            String newTitle = "New project " + i;
             Boolean newCompleted = false;
             Boolean newActive = false;
             String newDescription = "New Description";
@@ -75,6 +73,8 @@ public class ProjectsPerformanceTest {
             timeTable.add(time);
         }
         System.out.println(timeTable);
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc();
     }
 
     @Test
@@ -82,7 +82,7 @@ public class ProjectsPerformanceTest {
         List<Long> timeTable = new ArrayList<Long>();
         for(int i=0;i<1000;i++){
             JSONObject body = new JSONObject();
-            String newTitle = "New project " + String.valueOf(i);
+            String newTitle = "New project " + i;
             Boolean newCompleted = false;
             Boolean newActive = false;
             String newDescription = "New Description";
@@ -97,14 +97,17 @@ public class ProjectsPerformanceTest {
             timeTable.add(time);
         }
         System.out.println(timeTable);
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc();
     }
 
+    /*
     @Test
     public void createProjectsXmlPost(){
         List<Long> timeTable = new ArrayList<Long>();
         for(int i=0;i<1000;i++){
             JSONObject body = new JSONObject();
-            String newTitle = "New project " + String.valueOf(i);
+            String newTitle = "New project " + i;
             Boolean newCompleted = false;
             Boolean newActive = false;
             String newDescription = "New Description";
@@ -120,14 +123,18 @@ public class ProjectsPerformanceTest {
             timeTable.add(time);
         }
         System.out.println(timeTable);
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc();
     }
+
+     */
 
     @Test
     public void createProjectsXmlPut(){
         List<Long> timeTable = new ArrayList<Long>();
         for(int i=0;i<1000;i++){
             JSONObject body = new JSONObject();
-            String newTitle = "New project " + String.valueOf(i);
+            String newTitle = "New project " + i;
             Boolean newCompleted = false;
             Boolean newActive = false;
             String newDescription = "New Description";
@@ -143,7 +150,193 @@ public class ProjectsPerformanceTest {
             timeTable.add(time);
         }
         System.out.println(timeTable);
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc();
     }
+
+    @Test
+    public void amendProjectsJsonPost(){
+        List<Long> timeTable = new ArrayList<Long>();
+
+        for(int i=0;i<1000;i++){
+            JSONObject body = new JSONObject();
+            String newTitle = "New project " + i;
+            Boolean newCompleted = false;
+            Boolean newActive = false;
+            String newDescription = "New Description";
+            body.put("title",newTitle);
+            body.put("completed",newCompleted);
+            body.put("active",newActive);
+            body.put("description",newDescription);
+            Response response = RestAssured.given().contentType(ContentType.JSON).body(body.toString()).post(pUrl);
+        }
+
+        for(int i=1;i<=1000;i++){
+            JSONObject body = new JSONObject();
+            String newTitle = "modified project ";
+            Boolean newCompleted = true;
+            Boolean newActive = true;
+            String newDescription = "Modified Description";
+            body.put("title",newTitle);
+            body.put("completed",newCompleted);
+            body.put("active",newActive);
+            body.put("description",newDescription);
+            long start = System.nanoTime();
+            Response response = RestAssured.given().contentType(ContentType.JSON).body(body.toString()).post(pUrl+"/"+i);
+            long end = System.nanoTime();
+            long time = end-start;
+            timeTable.add(time);
+        }
+        System.out.println(timeTable);
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc();
+    }
+
+    @Test
+    public void amendProjectsJsonPut(){
+        List<Long> timeTable = new ArrayList<Long>();
+
+        for(int i=0;i<1000;i++){
+            JSONObject body = new JSONObject();
+            String newTitle = "New project " + i;
+            Boolean newCompleted = false;
+            Boolean newActive = false;
+            String newDescription = "New Description";
+            body.put("title",newTitle);
+            body.put("completed",newCompleted);
+            body.put("active",newActive);
+            body.put("description",newDescription);
+            Response response = RestAssured.given().contentType(ContentType.JSON).body(body.toString()).put(pUrl);
+        }
+
+        for(int i=1;i<=1000;i++){
+            JSONObject body = new JSONObject();
+            String newTitle = "modified project ";
+            Boolean newCompleted = true;
+            Boolean newActive = true;
+            String newDescription = "Modified Description";
+            body.put("title",newTitle);
+            body.put("completed",newCompleted);
+            body.put("active",newActive);
+            body.put("description",newDescription);
+            long start = System.nanoTime();
+            Response response = RestAssured.given().contentType(ContentType.JSON).body(body.toString()).put(pUrl+"/"+i);
+            long end = System.nanoTime();
+            long time = end-start;
+            timeTable.add(time);
+        }
+        System.out.println(timeTable);
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc();
+    }
+
+    @Test
+    public void amendProjectsXmlPost(){
+        List<Long> timeTable = new ArrayList<Long>();
+
+        for(int i=0;i<1000;i++){
+            JSONObject body = new JSONObject();
+            String newTitle = "New project " + i;
+            Boolean newCompleted = false;
+            Boolean newActive = false;
+            String newDescription = "New Description";
+            body.put("title",newTitle);
+            body.put("completed",newCompleted);
+            body.put("active",newActive);
+            body.put("description",newDescription);
+            Response response = RestAssured.given().contentType(ContentType.JSON).body(body.toString()).post(pUrl);
+        }
+
+        for(int i=1;i<=1000;i++){
+            JSONObject body = new JSONObject();
+            String newTitle = "modified project ";
+            Boolean newCompleted = true;
+            Boolean newActive = true;
+            String newDescription = "Modified Description";
+            body.put("title",newTitle);
+            body.put("completed",newCompleted);
+            body.put("active",newActive);
+            body.put("description",newDescription);
+            String xmlBody = XML.toString(body);
+            long start = System.nanoTime();
+            Response response = RestAssured.given().contentType(ContentType.XML).body(xmlBody).post(pUrl+"/"+i);
+            long end = System.nanoTime();
+            long time = end-start;
+            timeTable.add(time);
+        }
+        System.out.println(timeTable);
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc();
+    }
+
+    @Test
+    public void amendProjectsXmlPut(){
+        List<Long> timeTable = new ArrayList<Long>();
+
+        for(int i=0;i<1000;i++){
+            JSONObject body = new JSONObject();
+            String newTitle = "New project " + i;
+            Boolean newCompleted = false;
+            Boolean newActive = false;
+            String newDescription = "New Description";
+            body.put("title",newTitle);
+            body.put("completed",newCompleted);
+            body.put("active",newActive);
+            body.put("description",newDescription);
+            Response response = RestAssured.given().contentType(ContentType.JSON).body(body.toString()).put(pUrl);
+        }
+
+        for(int i=1;i<=1000;i++){
+            JSONObject body = new JSONObject();
+            String newTitle = "modified project ";
+            Boolean newCompleted = true;
+            Boolean newActive = true;
+            String newDescription = "Modified Description";
+            body.put("title",newTitle);
+            body.put("completed",newCompleted);
+            body.put("active",newActive);
+            body.put("description",newDescription);
+            String xmlBody = XML.toString(body);
+            long start = System.nanoTime();
+            Response response = RestAssured.given().contentType(ContentType.XML).body(xmlBody).put(pUrl+"/"+i);
+            long end = System.nanoTime();
+            long time = end-start;
+            timeTable.add(time);
+        }
+        System.out.println(timeTable);
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc();
+    }
+
+    @Test
+    public void deleteProjects(){
+        List<Long> timeTable = new ArrayList<Long>();
+
+        for(int i=0;i<1000;i++){
+            JSONObject body = new JSONObject();
+            String newTitle = "New project " + i;
+            Boolean newCompleted = false;
+            Boolean newActive = false;
+            String newDescription = "New Description";
+            body.put("title",newTitle);
+            body.put("completed",newCompleted);
+            body.put("active",newActive);
+            body.put("description",newDescription);
+            Response response = RestAssured.given().contentType(ContentType.JSON).body(body.toString()).post(pUrl);
+        }
+
+        for(int i=1;i<=1000;i++){
+            long start = System.nanoTime();
+            Response response = RestAssured.given().delete(pUrl+"/"+i);
+            long end = System.nanoTime();
+            long time = end-start;
+            timeTable.add(time);
+        }
+        System.out.println(timeTable);
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc();
+    }
+
 
 
 }
