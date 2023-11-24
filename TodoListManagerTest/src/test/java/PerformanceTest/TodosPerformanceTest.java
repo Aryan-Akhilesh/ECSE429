d10dbcdd5daf5da73546d0a85c0f6d368ed9944b
Lobo808 <112894176+Lobo808@users.noreply.github.com>
2023-11-24 13:53:54 -0500

    add final modifier to class field

--- TodoListManagerTest/src/test/java/PerformanceTest/TodosPerformanceTest.java
+++ TodoListManagerTest/src/test/java/PerformanceTest/TodosPerformanceTest.java
@@ -13,7 +13,7 @@ public class TodosPerformanceTest {
     private static final String pUrl = "http://localhost:4567/todos";
     private static ProcessBuilder pb;
     private final int[] populationSize = {1,10,50,100,200,500,1000};
-    private OperatingSystemMXBean osBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+    private final OperatingSystemMXBean osBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
     private String newTodoId = "1";
 
     @BeforeAll
